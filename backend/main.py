1b633e2b90a657e58c5384ba24061af6275fb2e1
17matb <147427088+17matb@users.noreply.github.com>
2025-11-21 16:10:03 +0100

    feat: provide POST /ping and GET /pings endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,39 @@
-def main():
-    print("Hello from backend!")
+from db import supabase
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"message": "api is fine"}
+
+
+@app.post("/ping")
+def ping(request: Request):
+    ip = request.client.host  # pyright: ignore[reportOptionalMemberAccess]
+    _ = supabase.table("pings").insert({"ip_address": ip}).execute()
+    return {"message": f"successfully added {ip} to db"}
+
+
+@app.get("/pings")
+def get_pings(page: int = 1, size: int = 3):
+    start = (page - 1) * size
+    end = start + size - 1
+    result = (
+        supabase.table("pings")
+        .select("*")
+        .order("timestamp")
+        .range(start, end)
+        .execute()
+    )
+    return result.data
