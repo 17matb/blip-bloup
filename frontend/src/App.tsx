994ad273fa02791b5cf9dda16ac8080d0b8609a7
17matb <147427088+17matb@users.noreply.github.com>
2025-11-21 13:20:59 +0100

    chore: init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <p className="text-2xl font-bold">blip-bloup</p>
+      </header>
+    </>
+  );
+}
+
+export default App;
